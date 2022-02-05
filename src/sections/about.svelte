301eabe7cce746ef042b41d4178f5a00708b7ee1
Przemek <przemek-szczepaniak@tlen.pl>
2022-02-05 15:59:26 +0100

    updated about section (WiP)

--- src/sections/about.svelte
+++ src/sections/about.svelte
@@ -1,16 +1,63 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import { fade } from 'svelte/transition';
+
+  let islogovisible = false;
+  onMount(() => {
+    const namecontainer = document.querySelector('#name');
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        islogovisible = !entry.isIntersecting;
+      });
+    });
+    observer.observe(namecontainer);
+  });
 </script>
 
+<div id="p-logo" class:logovisible={islogovisible} class:logoinvisible={!islogovisible}>P</div>
 <section id="about">
-  <h1 in:fade={{ duration: 200 }}>Hi, I'm <strong>Testing</strong></h1>
-  <h2 in:fade={{ duration: 200 }}>Front End Developer</h2>
+  <h2 in:fade={{ duration: 200 }}>Hello,</h2>
+  <h1 in:fade={{ duration: 200 }} id="name">
+    I'm <strong>Przemek</strong>,
+  </h1>
+  <h3 in:fade={{ duration: 200 }}>Front End Developer</h3>
 </section>
 
 <style>
+  #p-logo {
+    display: none;
+    z-index: 3;
+    position: fixed;
+    width: 120px;
+    top: 1px;
+    left: 0px;
+    height: 3rem;
+    line-height: 3rem;
+    font-size: 3rem;
+    font-family: 'Concert One', cursive;
+    font-weight: bold;
+    text-align: center;
+    transition: left 0.3s;
+    color: var(--text-color);
+    text-shadow: 1px 1px var(--background), 2px 2px var(--primary);
+    animation: logoAppear 0.3s;
+  }
+  @media (min-width: 800px) {
+    #p-logo {
+      display: initial;
+    }
+    #p-logo.logoinvisible {
+      animation: logoDisappear 0.3s forwards;
+    }
+    #p-logo.logovisible {
+      display: initial;
+      animation: logoAppear 0.3s;
+    }
+  }
   section {
-    min-height: calc(100vh - 100px);
+    min-height: calc(100vh);
     width: 100%;
+    padding: 100px 20px;
     display: flex;
     align-items: flex-start;
     justify-content: center;
@@ -18,19 +65,42 @@
     color: var(--text-color);
   }
   h1 {
-    height: 2em;
-    line-height: 2em;
+    height: 3rem;
+    line-height: 3rem;
+    font-size: 3rem;
     text-align: center;
-    font-size: 2em;
   }
   h1 strong {
     font-family: 'Concert One', cursive;
     font-weight: bold;
+    text-shadow: 1px 1px var(--background), 2px 2px var(--primary);
+    letter-spacing: 0.1rem;
   }
   h2 {
-    height: 1.6em;
-    line-height: 1.6em;
+    height: 2.4rem;
+    line-height: 2.4rem;
+    font-size: 2rem;
     text-align: center;
-    font-size: 1.6em;
+  }
+  @keyframes logoAppear {
+    0% {
+      transform: translateX(3rem);
+      opacity: 0;
+    }
+    100% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+  }
+  @keyframes logoDisappear {
+    0% {
+      transform: translateX(0);
+      opacity: 1;
+    }
+    100% {
+      transform: translateX(3rem);
+      opacity: 0;
+      display: none;
+    }
   }
 </style>
