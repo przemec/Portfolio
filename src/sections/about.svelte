13a5587ac50d1b7694cf25a655c40273078d368d
Przem ThinkBad <przemek-szczepaniak@o2.pl>
2022-02-09 18:52:02 +0100

    added my description in about section (styles WiP)

--- src/sections/about.svelte
+++ src/sections/about.svelte
@@ -1,33 +1,34 @@
 <script>
+  const age = Math.floor(new Date().getFullYear() - 2000.5);
 </script>
 
 <section id="about">
-  <h1>About</h1>
+  <h1>About Me</h1>
   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore consequatur tempore temporibus
-    obcaecati, sunt, consectetur reprehenderit ab quos ullam doloribus sed aut soluta. Odio ipsum
-    aspernatur excepturi dicta soluta accusamus.
+    I'm {age} years old Frond End Developer located in Cracow, Poland. 
+    My passion is building aesthetic, clean-looking, pixel-perfect websites.
   </p>
   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore consequatur tempore temporibus
-    obcaecati, sunt, consectetur reprehenderit ab quos ullam doloribus sed aut soluta. Odio ipsum
-    aspernatur excepturi dicta soluta accusamus.
+    I'm well-organised person who loves solving problems. 
+    My affection towards coding started in high school, where I wrote my first lines of JavaScript.
   </p>
   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore consequatur tempore temporibus
-    obcaecati, sunt, consectetur reprehenderit ab quos ullam doloribus sed aut soluta. Odio ipsum
-    aspernatur excepturi dicta soluta accusamus.
+    In the future I want to explore further Front End industry, as well as to learn more about Back End technologies, 
+    so that I can define myself as Full Stack Developer.
   </p>
 </section>
 
 <style lang="scss">
   section {
     min-height: 100vh;
-    margin-bottom: 20px;
+    padding: 30px 10px 20px;
     color: var(--text-color);
   }
   h1 {
-    width: 100%;
+    font-size: clamp(30px, 10vw, 45px);
     text-align: center;
+  }
+  p:not(:last-child) {
+    margin-bottom: 1rem;
   }
 </style>
