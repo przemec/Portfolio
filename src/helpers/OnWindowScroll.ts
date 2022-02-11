cbfe5b0564417ea42b1f92aafc4a7f3a0bf9cf67
Przem ThinkBad <przemek-szczepaniak@o2.pl>
2022-02-11 20:39:52 +0100

    updated header

--- src/helpers/OnWindowScroll.ts
+++ src/helpers/OnWindowScroll.ts
@@ -4,16 +4,16 @@ export default () => {
     const onwindowscroll = () => {
       let currentScrollPos = window.pageYOffset;
       if (prevScrollpos >= currentScrollPos) {
-        document.getElementsByTagName('header')[0].style.top = '0';
+        document.getElementsByTagName('header')[0].classList.toggle('ishidden', false);
       } else {
-        document.getElementsByTagName('header')[0].style.top = '-120px';
+        document.getElementsByTagName('header')[0].classList.toggle('ishidden', true);
       }
       if (currentScrollPos !== 0) {
         document.getElementById('scroll-tip').classList.toggle('scrolled', true);
-        document.getElementsByTagName('header')[0].classList.toggle('hasshadow', true);
+        document.getElementsByTagName('header')[0].classList.toggle('isscrolled', true);
       } else {
         document.getElementById('scroll-tip').classList.toggle('scrolled', false);
-        document.getElementsByTagName('header')[0].classList.toggle('hasshadow', false);
+        document.getElementsByTagName('header')[0].classList.toggle('isscrolled', false);
       }
       prevScrollpos = currentScrollPos;
     };
