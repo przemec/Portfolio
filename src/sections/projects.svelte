c05925432b41f7a37772b40fd53a92dc578e8d90
Przem ThinkBad <przemek-szczepaniak@o2.pl>
2022-02-28 14:10:09 +0100

    updated projects section (mobile view)

--- src/sections/projects.svelte
+++ src/sections/projects.svelte
@@ -1,33 +1,33 @@
 <script>
+  import Novum from '$lib/FeaturedProjects/Novum.svelte';
+  import PlaylistRandomizer from '$lib/FeaturedProjects/PlaylistRandomizer.svelte';
+  import Tbd from '$lib/FeaturedProjects/TBD.svelte';
 </script>
 
 <section id="projects">
-  <h1>Projects</h1>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore consequatur tempore temporibus
-    obcaecati, sunt, consectetur reprehenderit ab quos ullam doloribus sed aut soluta. Odio ipsum
-    aspernatur excepturi dicta soluta accusamus.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore consequatur tempore temporibus
-    obcaecati, sunt, consectetur reprehenderit ab quos ullam doloribus sed aut soluta. Odio ipsum
-    aspernatur excepturi dicta soluta accusamus.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore consequatur tempore temporibus
-    obcaecati, sunt, consectetur reprehenderit ab quos ullam doloribus sed aut soluta. Odio ipsum
-    aspernatur excepturi dicta soluta accusamus.
-  </p>
+  <h1>Featured Projects</h1>
+  <PlaylistRandomizer />
+  <Novum />
+  <Tbd />
 </section>
 
 <style lang="scss">
   section {
-    min-height: 100vh;
-    margin-bottom: 2rem;
+    position: relative;
+    width: 100%;
+    max-width: 100rem;
+    padding: 10vh 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
     color: var(--text-color);
   }
   h1 {
     width: 100%;
     text-align: center;
+    font-size: clamp(3.5rem, 10vw, 4.5rem);
+    margin-bottom: 2.5rem;
+    user-select: none;
   }
 </style>
