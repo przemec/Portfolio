739c2a59823cd2c570512628805fcac14d021b3b
Przem ThinkBad <przemek-szczepaniak@o2.pl>
2022-03-09 17:12:50 +0100

    updated contact section size

--- src/sections/contact.svelte
+++ src/sections/contact.svelte
@@ -23,8 +23,20 @@
 <style lang="scss">
   section {
     min-height: 100vh;
-    margin-bottom: 2rem;
+    min-height: calc(100vh - 14rem);
+    width: 100%;
+    max-width: 100rem;
+    padding: 0 0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
     color: var(--text-color);
+  }
+  @media (min-width: 800px) {
+    section {
+      min-height: calc(100vh - 8rem);
+    }
   }
   h1 {
     width: 100%;
