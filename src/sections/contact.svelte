9b79697c0a2f90ca69367721b8a6034e7bb60e09
Przem ThinkBad <przemek-szczepaniak@o2.pl>
2022-03-10 10:06:05 +0100

    implemented contact section

--- src/sections/contact.svelte
+++ src/sections/contact.svelte
@@ -1,23 +1,30 @@
 <script>
+  import Tooltip from '$lib/Tooltip.svelte';
+  import { tooltip } from '$interactions/tooltip';
 </script>
 
 <section id="contact">
-  <h1>Contact</h1>
+  <h1>Get In Touch</h1>
   <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore consequatur tempore temporibus
-    obcaecati, sunt, consectetur reprehenderit ab quos ullam doloribus sed aut soluta. Odio ipsum
-    aspernatur excepturi dicta soluta accusamus.
+    I'm currently looking for new opportunities in the web development industry. If You have any
+    questions, don't hesitate to contact me.
   </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore consequatur tempore temporibus
-    obcaecati, sunt, consectetur reprehenderit ab quos ullam doloribus sed aut soluta. Odio ipsum
-    aspernatur excepturi dicta soluta accusamus.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore consequatur tempore temporibus
-    obcaecati, sunt, consectetur reprehenderit ab quos ullam doloribus sed aut soluta. Odio ipsum
-    aspernatur excepturi dicta soluta accusamus.
-  </p>
+  <button
+    use:tooltip={{ content: Tooltip, text: 'Copy email to clipboard' }}
+    on:click={() => {
+      navigator.clipboard.writeText('pszczepaniak000@gmail.com');
+      document.querySelector('.tooltip-wrapper .content').textContent = 'Email has been copied!'
+    }}
+  >
+    <h2>pszczepaniak000@gmail.com</h2>
+    <svg viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z"
+      />
+    </svg>
+  </button>
+  <a href="mailto:pszczepaniak000@gmail.com">Let's talk!</a>
 </section>
 
 <style lang="scss">
@@ -39,7 +46,64 @@
     }
   }
   h1 {
-    width: 100%;
+    font-size: clamp(4rem, 8vw, 6rem);
+    margin-bottom: 1rem;
     text-align: center;
+  }
+  p {
+    max-width: 60rem;
+    font-size: clamp(1.8rem, 5vw, 2.6rem);
+    margin-bottom: 1rem;
+    text-align: center;
+  }
+  button {
+    position: relative;
+    cursor: pointer;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1rem;
+    margin-bottom: 1.8rem;
+    background: transparent;
+    border: none;
+    color: var(--text-color);
+    font-family: 'Baloo Paaji 2', Arial, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+      Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    h2 {
+      transition: color 0.2s ease-in-out;
+      font-size: clamp(2rem, 6vw, 3rem);
+      pointer-events: none;
+    }
+    svg {
+      transition: color 0.2s ease-in-out;
+      width: 3rem;
+      height: 3rem;
+      pointer-events: none;
+    }
+    &:hover h2,
+    &:focus-visible h2 {
+      color: var(--text-color-selected);
+    }
+    &:hover svg,
+    &:focus-visible svg {
+      color: var(--primary);
+    }
+  }
+  a {
+    text-decoration: none;
+    display: block;
+    padding: 0 1.5rem;
+    font-size: clamp(2rem, 7vw, 3rem);
+    color: var(--text-color);
+    border: 0.2rem solid currentColor;
+    border-radius: 1rem;
+    transition: color 0.3s ease-in-out;
+
+    &:hover,
+    &:focus-visible {
+      color: var(--primary);
+    }
   }
 </style>
