54ce4f65b70bea4e0fedeb3019da5f136fcb4445
Przemek <przemek-szczepaniak@tlen.pl>
2022-03-13 20:17:58 +0100

    added scroll triggers to skills and projects sections

---
+++ src/interactions/gsap-scroll-triggers.ts
@@ -0,0 +1,81 @@
+import gsap from 'gsap';
+
+export default () => {
+  let skills_timeline = gsap.timeline({
+    defaults: {
+      opacity: 0,
+      duration: 0.4
+    },
+    scrollTrigger: {
+      trigger: '#skills',
+      start: 'top 30%'
+    }
+  });
+  skills_timeline
+    .from('#skills h1', {
+      y: '-5rem'
+    })
+    .from('#skills-carousel .skills-wrapper', {
+      duration: 0.3,
+      stagger: -0.3
+    })
+    .from('#carousel-navigation .button-next', {
+      x: '-5rem'
+    })
+    .from(
+      '#carousel-navigation .button-prev',
+      {
+        x: '5rem'
+      },
+      '<'
+    );
+
+  let project_wrappers = document.querySelectorAll('.project-wrapper');
+  gsap.from('#projects h1', {
+    scrollTrigger: {
+      trigger: project_wrappers[0],
+      start: '-20rem center'
+    },
+    y: '-3rem',
+    opacity: 0,
+    duration: 0.4
+  });
+  project_wrappers.forEach((project) => {
+    let title = project.querySelector('h2');
+    let description = project.querySelector('p');
+    let links = project.querySelectorAll('.horizontal-section-wrapper');
+    let preview = project.querySelector('.project-preview');
+
+    let project_timeline = gsap.timeline({
+      defaults: {
+        opacity: 0,
+        duration: 0.4
+      },
+      scrollTrigger: {
+        trigger: project,
+        start: 'top center'
+      }
+    });
+    project_timeline
+      .from(title, {
+        y: '-2rem',
+        delay: 0.4
+      })
+      .from(
+        description,
+        {
+          y: '-2rem'
+        },
+        '<0.2'
+      )
+      .from(
+        links,
+        {
+          y: '-5rem',
+          stagger: 0.1
+        },
+        '<0.2'
+      )
+      .from(preview, { scale: 0.6 });
+  });
+};
--- src/interactions/init-interactions.ts
+++ src/interactions/init-interactions.ts
@@ -1,11 +1,18 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { updateHeaderDisplay, updateScrollTipDisplay } from '$interactions/scroll-events';
 
 export default async () => {
+  window.scrollTo(0, 0);
   let animatePageLoad = (await import('$interactions/gsap-page-load')).default;
+  let initScrollTriggers = (await import('$interactions/gsap-scroll-triggers')).default;
   let initActiveSectionObserver = (await import('$interactions/active-section-observer')).default;
 
-  window.scrollTo(0,0);
+  gsap.registerPlugin(ScrollTrigger);
+  initScrollTriggers();
+
   updateHeaderDisplay(0, false);
+
   const onPageLoadComplete = () => {
     initActiveSectionObserver();
 
